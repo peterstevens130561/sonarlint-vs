22810b633ad0337e38d020cc1b7ce3f5b9e68edf
Peter Stevens <peter.stevens@bakerhughes.com>
2015-09-11 14:41:03 +0200

    Initial commit for CommentedOutAssert

---
+++ src/Tests/SonarLint.UnitTest/BHI.TestCases/CommentedOutAssertExample.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Tests.Diagnostics
+{
+
+    class StringLocalizationExample
+    {
+        String myString = "12345"; //Noncompliant
+
+        static void Main(string[] args)
+        {
+            String mies = "mies"; //Noncompliant
+            String john = "aap" + mies; // Noncompliant
+            var myvar = "myvar"; // Noncompliant
+            var literal = @"mystring";
+            var myint = 4;
+            bool mybool = 6;
+            var someexpression = 4 + 5;
+            var somethingwithString = myvar.StartsWith("shouldBeInternationalized"); //Noncompliant
+            var somethingElse = myvar.StartsWith(@"shouldNotBeInternationalized"); 
+        }
+
+    }
+}
