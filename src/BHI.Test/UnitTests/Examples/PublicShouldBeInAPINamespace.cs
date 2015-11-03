73f76527e757382ab58e87d9ebd9168731a476d2
Peter Stevens <peter.stevens@bakerhughes.com>
2015-11-03 15:14:48 +0100

    Check with Public stuff

---
+++ src/BHI.Test/UnitTests/Examples/PublicShouldBeInAPINamespace.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace BHI.JewelEarth.Daffodil
+{
+
+    public class ShouldBeInApi
+    {
+        public void MyMethod()
+        {
+
+        }
+
+        void InternalMethod() { }
+
+        internal void AnotherInternalMethod()
+        {
+
+        }
+    }
+
+    public enum MyEnum
+    {
+        One,Two,Three
+
+    }
+
+    public interface PublicInterface
+    {
+
+    }
+}
